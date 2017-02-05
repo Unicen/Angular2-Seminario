88a93e245a5f0da00aedfb43341daa3867c4871e
Ignacio Jonas <ignaciojonas@gmail.com>
2017-02-05 19:40:48 -0300

    02-Components - Update Root Component

--- Brewery/src/app/app.component.ts
+++ Brewery/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-brewery',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 }
