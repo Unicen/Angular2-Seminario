88b46995dad4bdacf7cf7df0f62ecb20373ad79a
Ignacio Jonas <ignaciojonas@gmail.com>
2017-02-05 19:46:58 -0300

    02-Components - Create Beer List Component

---
+++ Brewery/src/app/beer-list/beer-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'beer-list',
+  templateUrl: './beer-list.component.html',
+  styleUrls: ['./beer-list.component.css']
+})
+export class BeerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
