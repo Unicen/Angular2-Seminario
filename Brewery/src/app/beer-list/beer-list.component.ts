22637e81365b2cc7528b4bae37f4b31a108581de
Ignacio Jonas <ignaciojonas@gmail.com>
2017-02-05 21:16:20 -0300

    03-DirectivesPipes - *ngFor in the list of beers

--- Brewery/src/app/beer-list/beer-list.component.ts
+++ Brewery/src/app/beer-list/beer-list.component.ts
@@ -6,12 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beer-list.component.css']
 })
 export class BeerListComponent implements OnInit {
-beer = {
+beers = [{
   name: "Negra Fuerte",
   style: "Porter",
   price: 20,
   stock: 1584
-}
+},
+{
+  name: "Red Red Wine",
+  style: "Barley Wine",
+  price: 40,
+  stock: 500
+},
+{
+  name: "La Rubia",
+  style: "Golden Ale",
+  price: 23.5,
+  stock: 121
+}]
+
   constructor() { }
 
   ngOnInit() {
