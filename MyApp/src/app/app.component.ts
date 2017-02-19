f2afffd2adf1d276ad2dbefde0ce8b9db515bb28
Sebastián González <sgonzalez@summasolutions.net>
2017-02-19 13:30:25 -0300

    Added scss support, created component.

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+}
+
+@Component({
+  selector: 'beer-list',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class Beerlist {
+}
+
+@Component({
+  selector: 'beer-cart',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class Beercart {
 }
--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BeerCartComponent } from './beer-cart/beer-cart.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerCartComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule,
