c79bc2c9f5c964a087d712bce8951e202d2d72b9
Ignacio Jonas <ignaciojonas@gmail.com>
2017-02-19 23:51:05 -0300

    07- Routing - Added Routing + About Component

--- Brewery/src/app/app.module.ts
+++ Brewery/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
 import { BeerDataService } from './beer-data.service';
+import { appRoutes } from './app.routes';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ BeerDataService ],
   bootstrap: [AppComponent]
